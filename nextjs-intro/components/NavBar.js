9396ed70935171966528a558d50cdc3e28c8b59c
Yeongmin_Chae <codudals98@naver.com>
2022-07-01 17:37:17 +0900

    connect next.js with css part1 use the css module ( which nicholas doesn't use a lot )

--- nextjs-intro/components/NavBar.js
+++ nextjs-intro/components/NavBar.js
@@ -1,18 +1,28 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import styles from "./NavBar.module.css";
 
 export default function NavBar() {
   const router = useRouter();
   return (
+    // <nav className={styles.nav}>
     <nav>
-      {/* <a href="/">Home</a>
-      <a href="/about">About</a> */}
-      {/* in nextJs we don't use anchor(a) bc it makes us to refresh the whole page instead of anchor nextjs is using Link */}
       <Link href="/">
-        <a style={{ color: router.pathname === "/" ? "red" : "blue" }}>Home</a>
+        <a
+          className={`${styles.link} ${
+            router.pathname === "/" ? styles.active : ""
+          }`}
+        >
+          Home
+        </a>
       </Link>
       <Link href="/about">
-        <a style={{ color: router.pathname === "/about" ? "red" : "blue" }}>
+        <a
+          className={[
+            styles.link,
+            router.pathname === "/about" ? styles.active : "",
+          ].join(" ")}
+        >
           About
         </a>
       </Link>
