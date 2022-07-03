593ec32aadc56f54bc4a48454ddc65f3ffe7df44
Yeongmin_Chae <codudals98@naver.com>
2022-07-03 15:25:39 +0900

    wear the styles (css)

--- movie_project_nextJs/components/NavBar.js
+++ movie_project_nextJs/components/NavBar.js
@@ -15,11 +15,30 @@ export default function NavBar() {
         </Link>
       </div>
       <style jsx>{`
-        a {
-          text-decoration: none;
+        nav {
+          display: flex;
+          gap: 10px;
+          flex-direction: column;
+          align-items: center;
+          padding-top: 20px;
+          padding-bottom: 10px;
+          box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -10px,
+            rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+        }
+        img {
+          max-width: 100px;
+          margin-bottom: 10px;
+        }
+        nav a {
+          font-weight: 300;
+          font-size: 18px;
         }
         .active {
           color: tomato;
+        }
+        nav div {
+          display: flex;
+          gap: 10px;
         }
       `}</style>
     </nav>
--- movie_project_nextJs/pages/index.js
+++ movie_project_nextJs/pages/index.js
@@ -11,14 +11,37 @@ export default function Home() {
     // async is annominous function , and this function has to be excuted immediately so i called the function with '()'
   }, []);
   return (
-    <div>
+    <div className="container">
       <Seo title="Home" />
       {!movies && <h4>Loading...</h4>}
       {movies?.map((movie) => (
-        <div key={movie.id}>
+        <div className="movie" key={movie.id}>
+          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
           <h4>{movie.original_title} </h4>
         </div>
       ))}
+      <style jsx>{`
+        .container {
+          display: grid;
+          grid-templage-columns: 1fr 1fr;
+          padding: 10px;
+          gap: 20px;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          // transition: <property> <duration> <timing-function> <delay>;
+          box-shadow: rgba(0, 0, 0, 5) 0px 4px 12px;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
     </div>
   );
 }
