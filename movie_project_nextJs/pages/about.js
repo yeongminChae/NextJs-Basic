03dd03d58e11f4eca030ab29d757600f283f905d
Yeongmin_Chae <codudals98@naver.com>
2022-07-03 12:51:33 +0900

    made the movie project app and worked some nextJs patterns

---
+++ movie_project_nextJs/pages/about.js
@@ -0,0 +1,10 @@
+import Seo from "../components/Seo";
+
+export default function potato() {
+  return (
+    <div>
+      <Seo title="About" />
+      <h1>About</h1>
+    </div>
+  );
+}
---
+++ movie_project_nextJs/pages/index.js
@@ -0,0 +1,10 @@
+import Seo from "../components/Seo";
+
+export default function Home() {
+  return (
+    <div>
+      <Seo title="Home" />
+      <h1 className="active">Home</h1>
+    </div>
+  );
+}
