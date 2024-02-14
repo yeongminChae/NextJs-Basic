ba7626e9f2f92cc3807b6a903b57996c17efac8d
Yeongmin_Chae <codudals98@naver.com>
2024-02-14 17:09:25 +0900

    next-js에서 not found page 란?

---
+++ nextjs14/app/not-found.tsx
@@ -0,0 +1,10 @@
+import Navigation from "../components/navigation";
+
+export default function AboutUs() {
+  return (
+    <div>
+      <Navigation />
+      <h1>404 Error.</h1>;
+    </div>
+  );
+}
