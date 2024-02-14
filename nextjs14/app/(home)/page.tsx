529d061454c1efc9eb1658e1e3b0a2d5ea12e1f7
Yeongmin_Chae <codudals98@naver.com>
2024-02-14 23:16:06 +0900

    React에서 데이터 fetching 하는 방법

--- nextjs14/app/(home)/page.tsx
+++ nextjs14/app/(home)/page.tsx
@@ -1,11 +1,27 @@
-export const metadata = {
-  title: "Home",
-};
+"use client";
+
+import { useEffect, useState } from "react";
+
+// export const metadata = { // use client에서는 metadata를 사용할 수 없다.
+//   title: "Home",
+// };
 
 export default function Page() {
-  return (
-    <div>
-      <h1>hello world</h1>
-    </div>
-  );
+  const [isLoading, setIsLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = async () => {
+    const response = await fetch(
+      "https://nomad-movies.nomadcoders.workers.dev/movies"
+    );
+    const json = await response.json();
+    setMovies(json);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  return <div>{!isLoading ? JSON.stringify(movies) : "Loading..."}</div>;
 }
