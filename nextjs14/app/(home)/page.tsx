23603000facf1d24dac60381971febdc5b2e0506
Yeongmin_Chae <codudals98@naver.com>
2024-02-18 22:47:13 +0900

    홈화면 스타일링 및 컴포넌트 분리

--- nextjs14/app/(home)/page.tsx
+++ nextjs14/app/(home)/page.tsx
@@ -1,6 +1,8 @@
-import Link from "next/link";
+import { Metadata } from "next";
+import Movie from "../../components/movie";
+import styles from "../../styles/home.module.css";
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Home",
 };
 
@@ -15,11 +17,14 @@ async function getMovies() {
 export default async function HomePage() {
   const movies = await getMovies();
   return (
-    <div>
+    <div className={styles.container}>
       {movies.map((movie) => (
-        <li key={movie.id}>
-          <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
-        </li>
+        <Movie
+          key={movie.id}
+          id={movie.id}
+          poster_path={movie.poster_path}
+          title={movie.title}
+        />
       ))}
     </div>
   );
