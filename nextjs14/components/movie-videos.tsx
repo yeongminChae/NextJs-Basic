9bf541574f1f57de04d1442b6e8779d698bb395f
Yeongmin_Chae <codudals98@naver.com>
2024-02-21 15:01:35 +0900

    작업도중 백업.

--- nextjs14/components/movie-videos.tsx
+++ nextjs14/components/movie-videos.tsx
@@ -1,23 +1,63 @@
-import { API_URL } from "../app/(home)/page";
+"use client";
+
+import { useEffect, useState } from "react";
+import styles from "../styles/movie-videos.module.css";
+import ArrowDirection from "./arrow";
 
 async function getVideos(id: string) {
-  console.log(`Fetching movie with id: ${Date.now()}`);
-  await new Promise((resolve) => setTimeout(resolve, 2000));
+  const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
   const response = await fetch(`${API_URL}/${id}/videos`);
   return response.json();
 }
 
-export default async function MovieVideos({ id }: { id: string }) {
-  const video = await getVideos(id);
-  return <h6>{JSON.stringify(video)}</h6>;
-  //   return (
-  //     <div>
-  //       {videos.map((video: any) => (
-  //         <div key={video.id}>
-  //           <h3>{video.name}</h3>
-  //           <p>{video.type}</p>
-  //         </div>
-  //       ))}
-  //     </div>
-  //   );
+export default function MovieVideos({ id }: { id: string }) {
+  const [videos, setVideos] = useState([]);
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    const fetchVideos = async () => {
+      const fetchedVideos = await getVideos(id);
+      setVideos(fetchedVideos);
+    };
+
+    fetchVideos();
+  }, [id]);
+
+  const lastPage = Math.floor(videos.length / 4);
+
+  const increasePage = () => {
+    setPage((prev) => (prev === lastPage ? 0 : prev + 1));
+  };
+
+  const decreasePage = () => {
+    setPage((prev) => (prev === 0 ? lastPage : prev - 1));
+  };
+
+  return (
+    <div className={styles.container}>
+      <ArrowDirection onClick={decreasePage} direction="left" />
+      <>
+        {videos.length === 0 ? (
+          <div className={styles.loadingBox}>
+            {[0, 1, 2, 3].map((i) => (
+              <div key={i} className={styles.box}>
+                <h1>Loading...{i + 1}</h1>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div className={styles.slider}>
+            {videos.slice(page * 4, (page + 1) * 4).map((video) => (
+              <iframe
+                key={video.id}
+                src={`https://youtube.com/embed/${video.key}`}
+                title={video.name}
+              />
+            ))}
+          </div>
+        )}
+      </>
+      <ArrowDirection onClick={increasePage} direction="right" />
+    </div>
+  );
 }
