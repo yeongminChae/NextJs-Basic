95ddc03c12407d90195f5915bd2a1ed930900f58
Yeongmin_Chae <codudals98@naver.com>
2024-03-02 23:44:41 +0900

    movie component 적용

--- nextjs14/components/movie-similar.tsx
+++ nextjs14/components/movie-similar.tsx
@@ -1,6 +1,6 @@
 import { API_URL } from "../app/(home)/page";
 import Movie from "./movie";
-import styles from "../styles/home.module.css";
+import styles from "../styles/home.module.scss";
 
 const getMovieSimilar = async (id: string) => {
   const response = await fetch(`${API_URL}/${id}/similar`);
@@ -11,18 +11,14 @@ const MovieSimilar = async ({ id }: { id: string }) => {
   const similarMovies = await getMovieSimilar(id);
 
   return (
-    <div>
+    <div className={styles.container}>
       {similarMovies.map((movie) => (
-        <div className={styles.container}>
-          {similarMovies.map((movie) => (
-            <Movie
-              key={movie.id}
-              id={movie.id}
-              poster_path={movie.poster_path}
-              title={movie.title}
-            />
-          ))}
-        </div>
+        <Movie
+          key={movie.id}
+          id={movie.id}
+          poster_path={movie.poster_path}
+          title={movie.title}
+        />
       ))}
     </div>
   );
